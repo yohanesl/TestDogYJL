5269c91c7022ac00a2f9e8cbc88c66fedb22ac06
Yohanes Lee <hniclee@gmail.com>
2016-04-06 12:07:02 -0400

    Initial Commit

--- TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
+++ TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
@@ -2,19 +2,51 @@ package edu.saintjoe.cs.brianc.testdogblc;
 
 public class TestDogBLC {
 
+	// This represents the dog's name and is a string object.
 	    private String name;
 	    
+	   // This represents the dog size using weight 
 	    private int weight;
-	    	    
+	    
+	    public static void main(String[] args) {
+	    	Dog dog1 = new Dog();
+	    	Dog dog2;
+	    	dog1.bark();
+	    	
+	    	dog1.name = "Buck";
+	    	dog1.setName = ("Buck");
+	    	dog2 = dog1;
+	    	dog2.bark();
+	    	dog2.setName = ("Chuck");
+	    	dog2.bark();
+	    	dog1.setWeight(100);
+	    	dog2.bark();
+	    	dog1.bark();
+	    	
+	    	Dog[] myDogs = new Dog[3];
+	    	myDogs[0] = new Dog();
+	    	myDogs[1] = new Dog();
+	    	myDogs[2] = dog1;
+	    	
+	    	myDogs[0].setName = ("Apollo");
+	    	myDogs[0].setWeight(55);
+	    	myDogs[1].setName = ("Creed");
+	    	myDogs[1].setWeight(33);
+	    	myDogs[0].bark();
+	    }
+	    	
+	    // Instance methods start here
+	    
+	    // There's one getter and one setter for each instance variable in a standard class
 	    public void setName(String newName) {
 	    	name = newName;
 	    }
-	    
+	    // the parameter newWeight is sent in to this method
 	    public void setWeight(int newWeight) {
 	    	weight = newWeight;
 	    }
 	    
-
+// Returns info back to the caller
 	    public String getName() {
 	    	return name;
 	    }
@@ -25,6 +57,8 @@ public class TestDogBLC {
 	    
 	    public void bark() {
 	    
+	    	// Declares a variable with local scope to the bark() method
+	    	// Initial value must be set by the programmer
 	    	int pointless = 0;
 	    	
 	    	
@@ -36,8 +70,10 @@ public class TestDogBLC {
 	    		System.out.println("Yip!! Yip!! says " + name + " that weighs " + weight);
 	    }
 	    
+	    // Use of multiple parameters
 	    public int setBoth(String newName, int newWeight) {
 	    	
+	    	// The use of a local variable
 	    	int oldWeight = weight;
 	    	
 	    	name = newName;
